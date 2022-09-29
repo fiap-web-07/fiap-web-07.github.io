4fcb5266811b856dfd5d47258e69685e8f6105b8
Beatriz Attorre <beatriz.m.attorre@avanade.com>
2022-09-29 09:10:40 -0300

    'Deploy'

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// trabalhando com JS em um arquivo externo
+
+console.log(nome, "chamando variável criada com var");
+
+var nome = "Beatriz";
+
+escreve('Exemplo')
+
+function escreve(titulo){
+    alert(titulo);
+}
